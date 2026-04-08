cc6a3c7cf0ae88584356a5ee0c69ece745772f05
aashikofficial-max <aashikofficial.as@gmail.com>
2026-04-08 09:58:57 +0530

    UC-8

--- App/src/TrainConsistService.java
+++ App/src/TrainConsistService.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class TrainConsistService {
+
+    public List<Bogie> filterBogiesByCapacity(List<Bogie> bogies, int threshold) {
+
+        return bogies.stream()
+                .filter(b -> b.getCapacity() > threshold)
+                .collect(Collectors.toList());
+    }
 }
